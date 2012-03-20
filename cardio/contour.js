0ffcf24e166fc8ee7554731311e49d2968d7d6b1
Vincent Woo <me@vincentwoo.com>
2012-03-19 22:24:31 -0700

    draft one for card.io hw

--- cardio/contour.js
+++ cardio/contour.js
@@ -1,5 +1,27 @@
 var data = [[0, 10, 0],
-  [10, 20, 10],
-  [0, 10, 0]];
+            [10, 20, 10],
+            [0, 10, 0]];
+var interval = 5;
+var max = -Infinity, min = Infinity;
+var canvas = document.getElementById('canvas');
+
+console.log(canvas);
+
+data.forEach (function(row) {
+  row.forEach (function(val) {
+    if (val > max) max = val;
+    if (val < min) min = val;
+  });
+});
+
+for (var threshold = min; threshold < max; threshold += interval) {
+  marching_squares(data.map (function(row) {
+    return row.map (function(val) {
+      return (val > threshold) ? 1 : 0;
+    });
+  }));
+}
+
+function marching_squares(filtered) {
   
-  
+}
