02a04d22302edc82b93c54bf672cb08c5e47eacb
Vincent Woo <me@vincentwoo.com>
2013-02-28 03:45:33 -0800

    Working solution

--- affirm/honeycomb.rb
+++ affirm/honeycomb.rb
@@ -21,18 +21,22 @@ def num_to_coords num
   return Vector[0, 0] if num == 1
 
   ring = num_to_ring num
-
   max_num = ring_to_max_num ring
-  side_length = ring
 
-  side_number = (max_num - num) / side_length
-  side_offset = (max_num - num) % side_length
+  side_number = (max_num - num) / ring
+  side_offset = (max_num - num) % ring
 
   translation = SIDE_TRANSLATIONS[side_number]
   transition = SIDE_TRANSLATIONS[(side_number + 1) % 6] - translation
-  (translation * side_length) + (transition * side_offset)
+  (translation * ring) + (transition * side_offset)
 end
 
-for i in 1..19
-  puts "#{i}:\t#{num_to_coords(i)}"
+def distance_between num1, num2
+  delta = num_to_coords(num1) - num_to_coords(num2)
+  delta = -1 * delta if delta.all? {|i| i < 0}
+  delta.all? {|i| i > 0} ? delta.max : delta.max - delta.min
 end
+
+num1 = ARGV[0].to_i
+num2 = ARGV[1].to_i
+puts "distance between #{num1} and #{num2} is #{distance_between num1, num2}"
