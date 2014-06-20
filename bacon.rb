34e1a7f9d125a202bea0874241fddc66384891a7
Vincent Woo <me@vincentwoo.com>
2014-06-20 02:30:46 -0700

    use typhoeus

--- bacon.rb
+++ bacon.rb
@@ -1,12 +1,17 @@
 require 'nokogiri'
-require 'open-uri'
+require 'typhoeus'
 
 WIKIPEDIA_ROOT = 'http://en.wikipedia.org/wiki/'
-
 BACON = 'Kevin_Bacon'
 
 def get_links(url)
-  doc = Nokogiri::HTML(open("#{WIKIPEDIA_ROOT}#{url}"))
+  begin
+    html = Typhoeus.get("#{WIKIPEDIA_ROOT}#{url}").body
+    doc = Nokogiri::HTML(html)
+  rescue
+    puts 'Retrying...'
+    retry
+  end
 
   doc.css('#mw-content-text a').select { |link|
     link.attributes['href'] && !link.attributes['class']
@@ -30,18 +35,18 @@ count = 0
 
 while path = queue.shift
   node = path.last
-  next if seen[node]
-  seen[node] = true
 
   count += 1
-  puts "(#{count}/#{queue.length})\t#{node}"
+  puts "(#{count}/#{queue.length})[#{path.length}]\t#{node}"
   links = get_links(node)
 
   if links.include? BACON
-    puts '*' * 20
-    puts path, BACON
-    puts '*' * 20
+    puts '*' * 20, path, BACON
     break
+  end
+
+  links.select! do |link|
+    !seen[link].tap { seen[link] = true }
   end
 
   queue += links.map { |link| path + [link] }
